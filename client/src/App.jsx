379f4016bdc48f7e0f3272d69ce2a62163cd81cb
Tanish <tanishpashte.24@gmail.com>
2025-06-13 23:05:25 +0530

    setup frontend using vite, react and tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// No App.css import needed here or in main.jsx if you deleted it earlier
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to EzLabs!
+      </h1>
+      {/* We'll add the "Get Started" button later when we build actual components */}
+    </div>
+  );
+}
+
+export default App;
